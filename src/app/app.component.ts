ad80dcdc6ed177c74034306ac07d47679c7851ab
Pasindu Kalubowila <pasindu66deshan@gmail.com>
2021-05-29 20:06:09 +0530

    Event Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   t = 'Data Binding';
+
+  handleEvent(){
+    console.log('Button Clicked')
+  }
+  handleEvent2(){
+    console.log('key Down')
+  }
 }
